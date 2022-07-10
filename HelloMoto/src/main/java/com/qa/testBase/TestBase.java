49b231f05f777a0f98968f2df5a1c55f7c1926f9
Parag Yawale <yawale.parag@gmail.com>
2022-07-10 18:36:17 +0530

    Created testBase package
    i have created setup() method

--- HelloMoto/src/main/java/com/qa/testBase/TestBase.java
+++ HelloMoto/src/main/java/com/qa/testBase/TestBase.java
@@ -1,19 +1,45 @@
 package com.qa.testBase;
 
-import org.junit.Test;
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.opera.OperaDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TestBase {
 	
-	@Test
-	public void m1() {
-		System.setProperty("webdriver.chrome.driver", "D:\\SetUps\\Selenium\\Newer_versions\\chromedriver.exe");   
-		WebDriver driver = new ChromeDriver();
-	
-	
-	
-	
+	WebDriver driver;
+	public void setUp() {
+		
+		String br = "chrome";
+	 if(br.equalsIgnoreCase("chrome")) {
+		WebDriverManager.chromedriver().setup();
+	      driver = new ChromeDriver();
 	}
-	
-}
+	 else if(br.equalsIgnoreCase("opera")) {
+		 WebDriverManager.operadriver().setup();
+		  driver = new OperaDriver(); 
+	 }
+	 else if(br.equalsIgnoreCase("edge")) {
+		  driver = new EdgeDriver();
+	 }
+	 else {
+		 System.out.println("Enter valid browser name");
+	 }
+	 
+	 driver.manage().window().maximize();
+	 driver.manage().deleteAllCookies();
+	 driver.get("https://kite.zerodha.com/?next=%2Fholdings");
+	 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+	 
+	}
+	 public void tearDown() {
+		driver.quit(); 
+	 }
+	 
+ }
+
