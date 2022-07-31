a9d5cb65618bcd9e722158a0b23f238e1c0fd60d
Parag Yawale <yawale.parag@gmail.com>
2022-07-31 19:53:52 +0530

    Added new
    hi i have added new class

---
+++ HelloMoto/src/main/java/com/qa/PageLayer/DashBoard.java
@@ -0,0 +1,19 @@
+package com.qa.PageLayer;
+
+import org.openqa.selenium.support.PageFactory;
+
+import com.qa.testBase.TestBase;
+
+public class DashBoard extends TestBase {
+
+	
+	
+	
+	public DashBoard() {
+		PageFactory.initElements(driver, this);
+	}
+	
+	
+	
+	
+}
